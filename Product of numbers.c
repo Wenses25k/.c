dce25b042a46e84ea6c2012f9ec9afe57779ec41
Wenses25k <102543943+Wenses25k@users.noreply.github.com>
2022-03-29 17:26:52 +0300

    Add files via upload

---
+++ Product of numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int product;
+    printf("Enter first number");
+    scanf("%d",&a);
+    printf("Enter second number");
+    scanf("%d",&b);
+
+    product=a*b;
+
+    printf("%d\n",product);
+    return 0;
+}
